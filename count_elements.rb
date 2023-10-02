a5a26280026e08f6a0f6ba8949c5d436d164d70f
hebiscus <107350293+hebiscus@users.noreply.github.com>
2023-10-02 18:30:53 +0200

    add my solution

--- count_elements.rb
+++ count_elements.rb
@@ -1,3 +1,20 @@
 class CountElements
+    def initialize(array)
+        @array = array
+    end
+
+    def perform
+        result_hash = Hash.new(0)
+        # loop over each element in the array; if result_hash has the element as a key increment += 1; otherwise element never occured before -> add it as a key with value 1
+        @array.each do |element|
+            if result_hash.key?(element)
+                result_hash[element] += 1
+            else 
+                result_hash[element] = 1
+            end
+        end
+
+        return result_hash
+    end
 
 end
--- validate_password.rb
+++ validate_password.rb
@@ -1,3 +1,25 @@
 class ValidatePassword
+    def initialize(string)
+        @string = string
+    end
 
+    def perform
+        capital_regex = /[A-Z]/
+        lowercase_regex = /[a-z]/
+        number_regex = /[0-9]/
+        symbol_regex = /[!@#\$%&*\+=:;?<>]/
+        repeated_regex = /(.)\1\1/
+        
+        # checking for every condition
+        if (@string.length > 6) && (@string.length < 26) &&
+            @string.match(capital_regex) &&
+            @string.match(lowercase_regex) &&
+            @string.match(number_regex) &&
+            @string.match(symbol_regex) &&
+            !@string.match(repeated_regex)
+            return true
+        else 
+            return false
+        end
+    end 
 end
